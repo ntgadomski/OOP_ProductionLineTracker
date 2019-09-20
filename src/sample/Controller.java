c439ac5c78fd37d615a129ad56038207a1dda1e1
Nickolas Gadomski <spudnick79@gmail.com>
2019-09-20 19:12:54 -0400

    Added Buttons, Database, and CSS file created layout with Scene Builder

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/** Controller Class Handles the the outputs of buttons. */
 public class Controller {
+
+  /** add product button on production line tab. */
+  @FXML private Button addProduct;
+
+  /** record button on produce tab. */
+  @FXML private Button record;
+
+  /**
+   * Handles action when button is clicked Outputs to console.
+   *
+   * @param event ActionEvent Object
+   */
+  @FXML
+  void printOutput(ActionEvent event) {
+    System.out.println("Product Added");
+  }
+
+  /**
+   * Handles action when button is clicked Outputs to console.
+   *
+   * @param event ActionEvent object
+   */
+  @FXML
+  void display(ActionEvent event) {
+    System.out.println("Recorded");
+  }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,41 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+/**
+ * /** Production Line Tracker Project used with a database to help with organizing
+ * products. @Author Nickolas Gadomski
+ */
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  /**
+   * Sets title for project and sets stage.
+   *
+   * @param primaryStage sets stage
+   * @throws Exception throws the exception
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Production Line Tracker");
+    primaryStage.setScene(new Scene(root, 600, 400));
+    primaryStage.show();
+  }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  /**
+   * Helps with launching program.
+   *
+   * @param args standard parameter
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
