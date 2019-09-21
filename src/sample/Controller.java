cd671b23507cda11c35062afa38caa30302e0f34
Nickolas Gadomski <spudnick79@gmail.com>
2019-09-21 19:45:19 -0400

    Sprint 1 completed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,17 @@
 package sample;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
+import org.w3c.dom.ls.LSOutput;
 
 /** Controller Class Handles the the outputs of buttons. */
 public class Controller {
+
+  @FXML private ComboBox<Integer> comboBox;
 
   /** add product button on production line tab. */
   @FXML private Button addProduct;
@@ -16,20 +22,46 @@ public class Controller {
   /**
    * Handles action when button is clicked Outputs to console.
    *
-   * @param event ActionEvent Object
+   * @param event MouseEvent Object
    */
   @FXML
-  void printOutput(ActionEvent event) {
+  void printOutput(MouseEvent event) {
+
     System.out.println("Product Added");
   }
 
   /**
    * Handles action when button is clicked Outputs to console.
    *
-   * @param event ActionEvent object
+   * @param event MouseEvent object
    */
   @FXML
-  void display(ActionEvent event) {
+  void record(MouseEvent event) {
     System.out.println("Recorded");
+  }
+
+  /**
+   * Handles action when comboBox is clicked.
+   *
+   * @param event MouseEvent object
+   */
+  @FXML
+  void list(MouseEvent event) {}
+
+  /** Method to start the comboBox. */
+  @FXML
+  public void initialize() {
+    comboBox.setEditable(true);
+    comboBox.getItems().add(1);
+    comboBox.getItems().add(2);
+    comboBox.getItems().add(3);
+    comboBox.getItems().add(4);
+    comboBox.getItems().add(5);
+    comboBox.getItems().add(6);
+    comboBox.getItems().add(7);
+    comboBox.getItems().add(8);
+    comboBox.getItems().add(9);
+    comboBox.getItems().add(10);
+    comboBox.getSelectionModel().selectFirst();
   }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,10 +3,13 @@ package sample;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -24,7 +27,10 @@ public class Main extends Application {
    */
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    Parent root =
+        FXMLLoader.load(
+            getClass()
+                .getResource("sample.fxml")); // Error with FXML file check FXML add to controller
     primaryStage.setTitle("Production Line Tracker");
     primaryStage.setScene(new Scene(root, 600, 400));
     primaryStage.show();
