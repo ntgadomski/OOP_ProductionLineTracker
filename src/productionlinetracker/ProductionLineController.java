1c532b288b4f708bdc46ce26d51d39099712e7e4
spudnick79 <spudnick79@gmail.com>
2019-11-24 01:03:25 -0500

    Database Functionality and Password Sprint 3

--- src/productionlinetracker/ProductionLineController.java
+++ src/productionlinetracker/ProductionLineController.java
@@ -79,6 +79,7 @@ public class ProductionLineController {
     initializeDB();
     loadProductList();
     setUpProductLineTable();
+//    showProduction();
   }
 
   /** List of FX identities used for functionality. */
@@ -175,7 +176,7 @@ public class ProductionLineController {
     }
     addToProductionDB(productRecords);
     loadProductionLog();
-    showProduction();
+//    showProduction();
     System.out.println("Product Recorded");
   }
 
